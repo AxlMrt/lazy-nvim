58f0f29bd0355657a8fcb5924b3e4a213cc03802
Martin Axel <martin.axel@sfr.fr>
2023-11-30 11:04:06 +0100

    Update tab keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -17,6 +17,8 @@ keymap.set('n', '<C-a>', 'gg<S-v>G')
 
 -- New tab
 keymap.set('n', 'te', ':tabedit')
+keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
 
 -- Split window
 keymap.set('n', 'ss', ':split<Return><C-w>w')
